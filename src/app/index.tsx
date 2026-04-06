3c5b5487a5b621553677b6d1a0e67e630c9974d4
Marcus Gajozo <marcusgajozo@gmail.com>
2026-04-06 11:00:12 -0400

    feat(router): add protected home screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,10 @@
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View>
+    <SafeAreaView>
       <Text>Hello, world!</Text>
-    </View>
+    </SafeAreaView>
   );
 }
