abad1a5e4d1900a769dba6e51f58975dabf7a875
Marcus Gajozo <marcusgajozo@gmail.com>
2026-04-08 20:23:29 -0400

    feat(theme): define initial color palette

---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+export const theme = {
+  colors: {
+    primary: "#C68B98",
+    secondary: "#C68B98",
+    tertiary: "#F5EBE0",
+    neutral: " #7D7571",
+
+    warning: "#FEDBA3 ",
+    danger: "#BA1A1A",
+    success: "#7ED321",
+
+    black: "#201B18 ",
+  },
+} as const;
