9a0631bd4ff99bdd6378aeabdea87f1753375210
Marcus Gajozo <marcusgajozo@gmail.com>
2026-04-09 15:54:46 -0400

    refactor(icon): switch to Feather icons

--- src/shared/components/icon/index.tsx
+++ src/shared/components/icon/index.tsx
@@ -1,13 +1,13 @@
-import { FontAwesome } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
 import React from "react";
 
 interface IconProps {
-  name: React.ComponentProps<typeof FontAwesome>["name"];
+  name: React.ComponentProps<typeof Feather>["name"];
   size?: number;
   color?: string;
-  style?: React.ComponentProps<typeof FontAwesome>["style"];
+  style?: React.ComponentProps<typeof Feather>["style"];
 }
 
 export function Icon({ name, size = 24, color = "black", style }: IconProps) {
-  return <FontAwesome name={name} size={size} color={color} style={style} />;
+  return <Feather name={name} size={size} color={color} style={style} />;
 }
